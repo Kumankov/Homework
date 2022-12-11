742d432854777c70230f3a935d5d49bab2973b07
Ilya <kumankov@gmail.ru>
2022-12-11 19:49:05 +0300

    create program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n=4; string[] SourceArray={"hello","2","world",":-)"};
+//int n=4; string[] SourceArray={"1234", "1567", "-2", "computer science"};
+//int n=3; string[] SourceArray={"Russia", "Denmark", "Kazan"};
+string[] ResultArray = new string[n];
+int count=0;
+for (int i=0; i< n; i++)
+    {
+        if (SourceArray[i].Length <=3)
+        {
+            ResultArray[count]= SourceArray[i];
+            count++;
+        }       
+    }
+Console.Write($"[");
+for (int j=0; j<count; j++)
+    {
+        if (j==0)
+            Console.Write(ResultArray[j]);
+        else     
+            Console.Write($", {ResultArray[j]}");
+    }
+Console.WriteLine($"]");
+
