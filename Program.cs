74ded59ffc28242510680152465e4bac8807cf83
Ilya <kumankov@gmail.ru>
2022-12-11 19:57:47 +0300

    update readme and cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿int n=4; string[] SourceArray={"hello","2","world",":-)"};
+﻿//int n=4; string[] SourceArray={"hello","2","world",":-)"};
 //int n=4; string[] SourceArray={"1234", "1567", "-2", "computer science"};
 //int n=3; string[] SourceArray={"Russia", "Denmark", "Kazan"};
+int n=8; string[] SourceArray={"Rus", "Denmark", "Kazan", "123", "PC", "AI", "Рython", ":0"};
 string[] ResultArray = new string[n];
 int count=0;
 for (int i=0; i< n; i++)
