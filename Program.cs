ef88b720ddf550ce14277d6c997af4e4c43c125b
Ilya <kumankov@gmail.ru>
2022-12-14 19:59:30 +0300

    551

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
-﻿//int n=4; string[] SourceArray={"hello","2","world",":-)"};
-//int n=4; string[] SourceArray={"1234", "1567", "-2", "computer science"};
-//int n=3; string[] SourceArray={"Russia", "Denmark", "Kazan"};
+﻿// int n=4; string[] SourceArray={"hello","2","world",":-)"};
+// int n=4; string[] SourceArray={"1234", "1567", "-2", "computer science"};
+// int n=3; string[] SourceArray={"Russia", "Denmark", "Kazan"};
 int n=8; string[] SourceArray={"Rus", "Denmark", "Kazan", "123", "PC", "AI", "Рython", ":0"};
-string[] ResultArray = new string[n];
 int count=0;
 for (int i=0; i< n; i++)
     {
-        if (SourceArray[i].Length <=3)
-        {
-            ResultArray[count]= SourceArray[i];
+        if (SourceArray[i].Length <=3)          
             count++;
+    }
+string[] ResultArray = new string[count];
+int k=0;
+for (int j=0; j< n; j++)
+    {
+        if (SourceArray[j].Length <=3) 
+        {
+            ResultArray[k]= SourceArray[j];
+            k++;   
         }       
     }
 Console.Write($"[");
-for (int j=0; j<count; j++)
+for (int l=0; l<count; l++)
     {
-        if (j==0)
-            Console.Write(ResultArray[j]);
+        if (l==0)
+            Console.Write(ResultArray[l]);
         else     
-            Console.Write($", {ResultArray[j]}");
+            Console.Write($", {ResultArray[l]}");
     }
 Console.WriteLine($"]");
-
+Console.WriteLine(count);
